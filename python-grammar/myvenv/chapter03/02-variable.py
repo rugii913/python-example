d3852a44e16e1961a22e0b6ed23b774603d99f7b
rugii913 <rugii913@gmail.com>
2024-05-06 22:55:09 +0900

    feat: Python 주석, 자료형, 변수
    Python 주석, 자료형, 변수

---
+++ python-grammar/myvenv/chapter03/02-variable.py
@@ -0,0 +1,13 @@
+# 변수: 데이터를 저장할 공간
+## 변수에 값을 할당
+username = "python"
+age = 5
+print(username, age)
+
+## 재할당 - 변수에 저장된 데이터를 변경하기
+age = 6
+print(username, age)
+
+### 기존 변수의 값을 가져와서 변수에 다시 할당 가능
+age = age + 1
+print(username, age)
