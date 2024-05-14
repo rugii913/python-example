b83ac64059cc2fa31099a8c1682fffdf6afab7b0
rugii913 <rugii913@gmail.com>
2024-05-14 20:02:48 +0900

    feat: POST 메서드 예시, query string, path variable 이용 방법 등
    POST 메서드 예시, query string, path variable 이용 방법 등

--- django_basic/django_basic/urls.py
+++ django_basic/django_basic/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # urls.py는 url 요청 경로를 기반으로 어디로 보낼지, 어떤 함수를 실행시킬지 등을 정의해두는 파일
-from shortener.views import index, redirect_test
+from shortener.views import index, get_user
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls), # Django가 이미 만들어준 url
     path("", index, name="index"), # name은 redirect 등을 위해 필요한 이름
-    path("redirect", redirect_test),
+    path("users/<int:user_id>", get_user), # path variable 넘길 수 있음
 ]
--- django_basic/shortener/views.py
+++ django_basic/shortener/views.py
@@ -1,5 +1,7 @@
+from django.http.response import JsonResponse
 from shortener.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
@@ -9,12 +11,20 @@ def index(request):
     user = User.objects.filter(username="admin").first() # Django ORM 사용 예시
     # user = User.objects.get(username="admin") # Django ORM에서 get()은 반드시 한 개이며, 존재하는 것이어야 하므로 없을 경우 에러 발생함, 보통 validation 필요 시 사용
     email = user.email if user else "Anonymous User" # user가 있는 경우와 없는 경우(None) 구분해서 할당
-
-    print(request.user.is_authenticated)
-    if request.user.is_authenticated is False:
-        email = "Anonymous User"
     return render(request, "base.html", {"welcome_msg": f"Hello {email}"}) # 어떤 템플릿을 렌더링하는지 명시, JSON 형태의 데이터를 넘겨줄 수 있음
 
-def redirect_test(request): # 직접 사용하지 않더라도 request를 받도록 명시함 - 이 requeset에 많은 정보를 담고 있음
-    print("Do Redirect")
-    return redirect("index") # 여기서 "index"는 위 함수 식별자 index가 아니라 urls.py에 path() 중 name에 지정된 값
+@csrf_exempt # Django는 기본적으로 csrf 토큰을 발급, 요청 위변조 방지 - @csrf_exempt는 csrf 체크를 하지 말라는 것 - Postman으로 체크할 때는 이 데코레이터를 붙여줌
+# 아예 csrf 토큰 발급, 체크를 꺼두려면 settings.py 중 MIDDLEWARE = [..]에서 "...CsrfViewMiddleware" 부분을 주석 처리
+def get_user(request, user_id): # path variable을 받음 urls.py 참고
+    print(user_id)
+    if request.method == "GET":
+        abc = request.GET.get("abc") # query string을 알아서 파싱함
+        xyz = request.GET.get("xyz")
+        user = User.objects.filter(pk=user_id).first()
+        return render(request, "base.html", {"user": user, "params": [abc, xyz]})
+    elif request.method == "POST":
+        username = request.GET.get("username")
+        if username:
+            user = User.object.filter(pk=user_id).update(username=username)
+        return JsonResponse(status =201, data=dict(msg="You just reached with Post Method!")) # 템플릿으로 보내는 게 아니라 JSON으로 응답
+        # return JsonResponse(dict(msg="POST 메서드"), safe=False) # 한글이 포함된 경우 safe=False로 보내야 오류 방지
