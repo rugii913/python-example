d33defc8ae470b283d0b203ac1b510e1989e7e56
rugii913 <rugii913@gmail.com>
2024-05-14 18:46:05 +0900

    feat: urls.py, views.py, settings.py 수정, 템플릿 html 파일 작성
    - [프로젝트명] 디렉토리의 urls.py 수정
      - url 요청 경로를 어디로 연결할지 정의
    - [앱명] 디렉토리의 views.py 수정
      - url 요청 경로와 연결될 함수를 작성
      - 템플릿으로 연결시킬 수도 있고, 리다이렉트 시킬 수도 있음
    - 템플릿 html 파일 작성
      - 루트/templates 디렉토리에 위치시켰는데,
      - Django가 이를 연결시키려면 [프로젝트명]/settings.py에 템플릿이 있는 디렉토리를 명시해줘야 함

--- django_basic/django_basic/urls.py
+++ django_basic/django_basic/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# urls.py는 url 요청 경로를 기반으로 어디로 보낼지, 어떤 함수를 실행시킬지 등을 정의해두는 파일
+from shortener.views import index, redirect_test
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), # Django가 이미 만들어준 url
+    path("", index, name="index"), # name은 redirect 등을 위해 필요한 이름
+    path("redirect", redirect_test),
 ]
--- django_basic/shortener/views.py
+++ django_basic/shortener/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from shortener.models import User
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+# cf . templates 폴더에 있는 html을 렌더링하도록 연결해주려면,
+#       settings.py 파일 중 TEMPLATES 부분 중에 'DIRS': [os.path.join(BASE_DIR, "templates")], 로 수정해줘야 함
+def index(request):
+    user = User.objects.filter(username="admin").first() # Django ORM 사용 예시
+    # user = User.objects.get(username="admin") # Django ORM에서 get()은 반드시 한 개이며, 존재하는 것이어야 하므로 없을 경우 에러 발생함, 보통 validation 필요 시 사용
+    email = user.email if user else "Anonymous User" # user가 있는 경우와 없는 경우(None) 구분해서 할당
+
+    print(request.user.is_authenticated)
+    if request.user.is_authenticated is False:
+        email = "Anonymous User"
+    return render(request, "base.html", {"welcome_msg": f"Hello {email}"}) # 어떤 템플릿을 렌더링하는지 명시, JSON 형태의 데이터를 넘겨줄 수 있음
+
+def redirect_test(request): # 직접 사용하지 않더라도 request를 받도록 명시함 - 이 requeset에 많은 정보를 담고 있음
+    print("Do Redirect")
+    return redirect("index") # 여기서 "index"는 위 함수 식별자 index가 아니라 urls.py에 path() 중 name에 지정된 값
